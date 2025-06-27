5eeca28d80513e0f248da7523bf46fd530fe049c
Torgunidas <gabrielmarczak@gmail.com>
2025-06-28 00:48:03 +0200

    dialog_action_dont_work

--- scripts/dialogues/DialogAction.gd
+++ scripts/dialogues/DialogAction.gd
@@ -1,10 +1,12 @@
 extends Resource
 class_name DialogAction
 @export_enum("add_money","deduct_money","add_life",
-			 "start_quest","finish_quest",
-			 "start_animation",
-			 "close") var type := "close"
+						"start_quest","finish_quest",
+						"start_animation",
+						"close",
+						"npc_text") var type := "close"
 @export var amount     : int    = 0
 @export var quest_id   : String = ""
 @export var anim_name  : String = ""
 @export var loop_count  : int    = 1
+@export var npc_text_id : String = ""
