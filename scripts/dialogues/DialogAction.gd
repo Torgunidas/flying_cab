ea68927ae1a13b9e49e6a4055b89f18a848ce11a
Torgunidas <gabrielmarczak@gmail.com>
2025-07-13 22:35:32 +0200

    wip item system not working quest

--- scripts/dialogues/DialogAction.gd
+++ scripts/dialogues/DialogAction.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name DialogAction
-@export_enum("add_money","deduct_money","add_life","start_quest","finish_quest","start_animation","grant_vehicle_access","revoke_vehicle_access","grant_item","revoke_item","close","npc_text") var type := "close"
+@export_enum("add_money","deduct_money","add_life","start_quest","finish_quest","start_animation","grant_vehicle_access","revoke_vehicle_access","grant_item","revoke_item","close","npc_text") var type := "close" # gdlint:ignore=max-line-length
+
 @export var amount: int = 0
 @export var quest_id: String = ""
 @export var anim_name: String = ""
